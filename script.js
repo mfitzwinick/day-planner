eacea07719e10547e516523c75c37c9f8ad6f726
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2020-11-07 14:22:44 -0800

    inputing time from moment.js

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
-#Pseudo
+// $(document).ready(function(){ 
 
+// var queryURL = "https://www.timeanddate.com/worldclock/usa/los-angeles"
+
+
+const now=moment();
+console.log(now);
+$("#currentDay").text(now)
+
+
+
+// var currentDay = new Date($.now());
+// $("#currentDay").html(currentDay);
+// var dayContent = JSON.parse(localStorage.getItem("input"));
+// if(dayContent === null){
+//     dayContent = ["","","","","","","","","","","",""];
+// }
+// for(i = 9; i < 22; i++){
+//     $("#"+i.toString()).html(dayContent[i-9])
+//     if(i === currentDay.getHours()){
+//         $("#"+i.toString()).attr("class","form-control present");
+//     }
+//     if(i < currentDay.getHours()){
+//         $("#"+i.toString()).attr("class","form-control past");
+//     }
+//     if(i > currentDay.getHours()){
+//         $("#"+i.toString()).attr("class","form-control future");
+//     }
+// }
+// $(".saveBtn").on("click", function(event){
+//     var input = $("#" + event.target.value).val();
+//     dayContent.splice((event.target.value-9), 1, input)
+//     localStorage.setItem("input", JSON.stringify(dayContent)) 
+// })
+//__________________________________________________________
+// $.ajax({
+// url: queryURL,
+// method: "GET"
+// }).then(function(response) {
+//     console.log(response)
