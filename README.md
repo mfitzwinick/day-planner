f8f2050260ede635524180a3ab7676d1c629c65f
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2020-11-15 13:20:04 -0800

    updating script file, revised Readme

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
-# day-planner
+# Perfect Day Planner READ.ME
 
-This is a work in progress....it is not yet complete.
+# Description/Purpose
+
+The Perfect Day Planner is time-tracking tool that will allow the user to see past scheduled events, the present scheduled activities, and what may be coming up in the future.
+
+On any given day, the current DATE will load at the top of the page.  One hour time slots beginning at 9am and ending at 5pm provide the user with a place to input their daily work schedule.  A save button on the right hand side will ensure that the activity will remain on the page on subsequent days until it is updated by the user.
+
+The time slots are color coded gray, red and green; so that the user can easily see what scheduled items are past, present and future.
 
 
+## Technologies Used
+
+Languages used: 
+- HTML
+- CSS
+- JAVASCRIPT / JQUERY
+
+The site pulls from the following third-party APIs:
+- JQuery
+- Bootstrap
+- Moment.js
+
+
+## File Structure
+
+The site is built with an index html page
+The index.html is linked to: 
+-  style.css
+-  script.js
+
+### How to Access the Work
+
+I will be supplying a url to the project along with a git hub url.  
+
+### Acknowledgments
+
+I'd like to thank Bryan Swarthout and Wilson Lam at UCLA.  Thanks also to Dru Sanchez who helped problem solve a local storage issue, and who also provided guidance on syntax issues.
 
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //set date at the top of the page//
     const m = moment()
     console.log(m);
@@ -8,11 +8,11 @@ $(document).ready(function() {
     //set variable for current hour//
     const h = m.format("H");
     console.log(h)
-
+    //create an array to compare hour integers from moment to hour integers inside of the planner//
     let blockHour = []
     for (let i = 9; i < 18; i++) {
 
-        blockHour.push($(`#$ { i }`))
+        blockHour.push($(`#${i}`))
     }
 
     let timeSlot = blockHour.map(x => parseInt(x.attr("id")))
@@ -20,90 +20,28 @@ $(document).ready(function() {
     for (let i = 0; i < timeSlot.length; i++) {
         console.log(timeSlot[i])
         if (timeSlot[i] < parseInt(h)) {
-            $(`.hour$ { i }`).addClass("past")
+            $(`.hour${i}`).addClass("past")
         } else if (timeSlot[i] == parseInt(h)) {
-            $(`.hour$ { i }`).addClass("present")
+            $(`.hour${i}`).addClass("present")
         } else {
-            $(`.hour$ { i }`).addClass("future")
+            $(`.hour${i}`).addClass("future")
         }
     };
-    // for (let i = 0; i < 9; i++) {
-    //     $(`hour${i}`).value = localStorage.getItem([i])
-    // }
-    // $("hour0").value = localStorage.getItem('btn9')
-    //     if (localStorage!==null)
-    //     $("#scheduledItem9").html(scheduledItem); 
-    // }
+ 
+//create an event listener to place value of input into local storage//
+    $(".rightDiv").on("click", function () {
+        var hour = $(this).attr("data-hour");
+        localStorage.setItem(hour, $(`#${hour}`).val())
+    })
 
-    // $(".rightDiv").on("click", function() {
-    //             var storage = [];
-    //             var hour = $(this.attr("id"));
-    //             console.log(hour)
-    //                 //var letters = $(`#
-    //                 // $ { hour }
-    //             `).val();
-    //             // storage.push({
-    //             //     time: hour,
-    //             //     scheduled: letters
-    //             // });
-    //         localStorage.setItem({
-    //             text: (#midDiv.valu),
-    //             id: (this.id),
-    //         });
+//function that will pull inputs from local storage when reloading page//
+    function reloadInfo() {
+        for (let i = 9; i < 18; i++) {
+            $(`#hour${i}`).val(localStorage.getItem(`hour${i}`))
+        };
 
+    }
 
-    //         // function reloadInfo () {
-    //         // //     for (let i=0; localStorage.length; i++) {
-    //         //         var scheduledItem=JSON.parse(localStorage.getItem("text: "))
-    //         //         $("#" + scheduledItem.time).text(scheduledItem);
-    //         //       console.log(scheduledItem);
-    //         // };
-
-
-    //         //     }
-    //         // //     if ($("scheduledItem9")!==null) {
-    //         //         location.reload();
-    //         //     }
-    //         // reloadInfo ();
-
-
-
-    //         // #Pseudo Code------------------------
-    //         //1. pull out the hour from the time - WHERE FROM????
-    //         // - do I put time into local storage as an object?
-
-    //         //2. parse out the info with JSON and create a 
-    //         //variable to represent currentHour
-
-    //         //3. use if statements to compare Hour variable to Row TIME string
-    //         // code for each individual row?
-    //         //if a match, change color ie: (.attr("class" ".past"))
-
-    //         //5. on reload - check for local storage and re-populate
-
-    //         //--------------------------------------------------
-    //   
-
-    //         // Splice ( )
-    //         // Used for adding/removing elements from array.
-    //         // Returns an array of removed elements.
-    //         // Changes the array.
-    //         // For adding elements: array.splice (index, number of elements, element)
-    //         // For removing elements: array.splice (index, number of elements)
-    //         // Can only be used for arrays.
-
-
-    //         // var currentDay = new Date($.now());
-    //         // $("#currentDay").html(currentDay);
-    //         // var dayContent = JSON.parse(localStorage.getItem("input"));
-    //         // if(dayContent === null){
-    //         //     dayContent = ["","","","","","","","","","","",""];
-    //      
-    //         // }
-    //         // $(".saveBtn").on("click", function(event){
-    //         //     var input = $("#" + event.target.value).val();
-    //         //     dayContent.splice((event.target.value-9), 1, input)
-    //         //     localStorage.setItem("input", JSON.stringify(dayContent)) 
-    //         // })
+    reloadInfo()
 
 });
